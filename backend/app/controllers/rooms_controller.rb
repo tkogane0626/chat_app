c12116f1edb65417f125f5bc959cb8ca744753a3
tkogane0626 <pad.kokin@gmail.com>
2023-11-24 18:18:16 +0900

    チャットルーム管理APIの実装

---
+++ backend/app/controllers/rooms_controller.rb
@@ -0,0 +1,19 @@
+class RoomsController < ApplicationController
+  def index
+    rooms = Room.all
+
+    render(json: rooms, status: :ok)
+  end
+
+  def create
+    room = Room.create(room_params)
+
+    render(json: room, status: :ok)
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/api/test', to: 'application#test'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :rooms, only: [:index, :create]
 end
