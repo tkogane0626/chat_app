95c5ca6f3dfbc5c381ab3d945804dcfcd91eb74d
tkogane0626 <pad.kokin@gmail.com>
2023-11-26 00:20:56 +0900

    メッセージ送信機能の実装

--- frontend/src/components/ChatRoom.vue
+++ frontend/src/components/ChatRoom.vue
@@ -7,6 +7,20 @@
         <strong>{{ message.sender_name }}:</strong> {{ message.content }}
       </li>
     </ul>
+
+    <form @submit.prevent="sendMessage">
+      <div>
+        <h3>名前</h3>
+        <input type="text" v-model="senderName" placeholder="名前を入力" required />
+      </div>
+      <div>
+        <h3>メッセージ</h3>
+        <input type="text" v-model="newMessageContent" placeholder="メッセージを入力" required />
+      </div>
+      <div>
+        <button type="submit">送信</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -19,6 +33,8 @@ export default {
     return {
       roomName: '',
       messages: [],
+      senderName: '',
+      newMessageContent: ''
     };
   },
   created() {
@@ -35,6 +51,19 @@ export default {
           console.error(error);
         });
     },
+    sendMessage() {
+      axios
+        .post(`http://localhost:3000/rooms/${this.roomId}/messages`, {
+          content: this.newMessageContent,
+          sender_name: this.senderName
+        })
+        .then(() => {
+          this.newMessageContent = ''
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
   },
 };
 </script>
